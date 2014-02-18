82992aa31d5a8d50fe687765d057a25dad7775b3
Colin Merkel <colin.merkel@gmail.com>
2014-02-18 19:54:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grookle::Application.config.secret_token = '9cdc50642a2adb59538486b62c62ed6770d2ba647e1ecf30554593d257f66baeb0d26407191cabfc12579c614a17a6ba9db582318810c4986d57a5ed5c5f7160'
